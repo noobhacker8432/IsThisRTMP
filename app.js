e3a2271e168b37179893b8439d188196b6c4610a
noobhacker8432 <67953642+noobhacker8432@users.noreply.github.com>
2024-02-26 08:54:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
+
+
 const spawn = require('child_process').spawn;
 
 const keyRTMP = process.env.KEYRTMP //You Can Change the RTMP url as per your need. For Ex: Telegram, YouTube, Twitch etc.
